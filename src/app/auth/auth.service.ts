324866f5926a49f81482681cb5765f8ab53cc792
unknown <przemek1922@vp.pl>
2020-05-17 10:12:59 +0200

    Handle error sign up

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
-
-interface AuthResponseData {
+export interface AuthResponseData {
     idToken: string,
     email: string,
     refreshToken: string,
     expiresIn: string,
-    localId: string
+    localId: string,
+    registered?: boolean
 }
 
 @Injectable({providedIn: 'root'})
@@ -21,7 +23,25 @@ export class AuthService {
                 email: email,
                 password: password,
                 returnSecureToken: true
-            })
+            }).pipe(catchError(errorRes => {
+                let errorMessage = 'An uknown error occured!';
+                if(!errorRes.error || !errorRes.error.error) {
+                    return throwError(errorMessage);
+                }
+                switch(errorRes.error.error.message) {
+                    case 'EMAIL_EXISTS':
+                        errorMessage = 'The email address is already in use by another account';
+                }
+                return throwError(errorMessage);
+            }))
+    }
+
+    login(email: string, password: string){
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]', {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        })
     }
 
 }
